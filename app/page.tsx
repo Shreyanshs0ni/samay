53badf5e62cc8008d6b1dff3fd714040d0af3e16
Shreyansh Soni <shreyanshsoni07@gmail.com>
2026-04-01 16:28:36 +0530

    Setup Husky Pre-Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 const page = () => {
   return (
     <div className="text-red-400">
-      Hello World{' '}
-      <div>
-        {' '}
-        How are you? <div>I am fine</div>
-      </div>
+      Hello World
+      <div>How are you?</div>
+      <div>I am fine</div>
     </div>
   );
 };
