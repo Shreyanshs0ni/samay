c2153ff8070ce3c03af907a94835761bd5e74a7f
Shreyansh Soni <shreyanshsoni07@gmail.com>
2026-04-01 16:26:57 +0530

    Test Husky

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 const page = () => {
   return (
     <div className="text-red-400">
-      Hello World <div> How are you?</div>
+      Hello World{' '}
+      <div>
+        {' '}
+        How are you? <div>I am fine</div>
+      </div>
     </div>
   );
 };
