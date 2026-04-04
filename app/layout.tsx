5d8ca8e684b020bee3be305f8d908b934442c482
Shreyansh Soni <shreyanshsoni07@gmail.com>
2026-04-05 00:00:15 +0530

    Add a Navbar with just logo and clerk user button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
+import Navbar from '@/components/layout/Navbar';
 import './globals.css';
-import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
 
 export default function RootLayout({
   children,
@@ -9,22 +9,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-full flex flex-col">
-        <ClerkProvider>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <Show when="signed-out">
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </Show>
-            <Show when="signed-in">
-              <UserButton />
-            </Show>
-          </header>
-          {children}
-        </ClerkProvider>
+        <Navbar />
+        {children}
       </body>
     </html>
   );
---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,28 @@
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
+
+export default function Navbar() {
+  return (
+    <ClerkProvider>
+      <div className="flex justify-between p-4">
+        <div className="flex items-center">
+          <Image src="/logo.png" alt="logo" width="50" height="50" />
+          <h1>SAMAY</h1>
+        </div>
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+          <Show when="signed-out">
+            <SignInButton />
+            <SignUpButton>
+              <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                Sign Up
+              </button>
+            </SignUpButton>
+          </Show>
+          <Show when="signed-in">
+            <UserButton />
+          </Show>
+        </header>
+      </div>
+    </ClerkProvider>
+  );
+}
